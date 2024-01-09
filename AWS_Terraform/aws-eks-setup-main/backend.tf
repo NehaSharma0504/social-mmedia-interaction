472974d73431940579934ecc34ee34eed8dd4227
NehaSharma0504 <ns14996@gmail.com>
2024-01-09 16:16:48 +0530

    AWS services

--- AWS_Terraform/aws-eks-setup-main/backend.tf
+++ AWS_Terraform/aws-eks-setup-main/backend.tf
@@ -4,8 +4,8 @@ terraform {
     key    = "npglobalintegrationv2.tfstate"
     region = "us-east-1"
     profile = "default"
-    access_key = "AKIAQPGA7FTW2L6N4NE3"
-    secret_key = "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"
+    access_key = "xxx"
+    secret_key = "xxx"
   }
 }
 
